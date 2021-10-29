874ec2e3385a5cb0b7a6b06f3b5fdd2c4c1fadb9
Aethar <elliott.ashby88@gmail.com>
2021-10-29 21:53:18 +0100

    c4project - added minimax algorith!

--- programming/python/C4Project.py
+++ programming/python/C4Project.py
@@ -1,21 +1,229 @@
-# %%
-import os, sys
+import os, sys, math
 import numpy as np
 from matplotlib import pyplot as plt, image, colors
+import random
+from multiprocessing import Process
 
 # dict of players
 players = {
-    "red": 1,
-    "blue": 2
+    "red": 1.0,
+    "blue": 2.0
 }
 
 # defines number of rows and number of columns
 rows, columns = 6, 7
 
+class miniMax:
+
+    def boardScore(self, board, turn):
+        '''creates arrays of each row and column and awards it a score
+            args:
+                board (mxn matrix): only accepts values 0 to 4
+                turn (the current player in the form of a string): used to pass into partialBoard check
+            returns:
+                score: a total score assigned to the whole board
+        '''
+        score = 0
+
+        arrayMid = [int(i) for i in list((board[columns//2][:]))]
+        midCount = arrayMid.count(players[turn])
+        score += midCount * 6
+
+        for x in range(rows):
+            arrayRow = [int(i) for i in list(board[x][:])]
+            # print(arrayRow, end='    ')
+            for y in range(columns-3):
+                partialBoard = arrayRow[y:y+4]
+                score += self.partialBoardCheck(partialBoard, turn)
+
+        for y in range(columns):
+            arrayCol = [int(i) for i in list(board[:][y])]
+            # print(arrayCol, end='   ')
+            for x in range(rows-3):
+                partialBoard = arrayCol[x:x+4]
+                score += self.partialBoardCheck(partialBoard, turn)
+                # print(arrayCol)
+        # print(score)
+        for x in range(rows-3):
+            for y in range(columns-3):
+                partialBoard = [board[x+i-1][y+i-1] for i in range(4)]
+                score += self.partialBoardCheck(partialBoard, turn)
+
+        for x in range(rows-3):
+            for y in range(columns-3):
+                partialBoard = [board[x+3-i-1][y+i-1] for i in range(4)]
+                score += self.partialBoardCheck(partialBoard, turn)
+
+        return score
+
+    def partialBoardCheck(self, partialBoard, turn):
+        '''checks each array created by self.boardScore() and awards it a score
+
+            args:
+                partialBoard (list created by self.boardScore()): used to generate score
+                turn: used to count specific numbers in the arrays
+
+            Returns:
+                Score for the partialBoard
+        '''
+        score = 0
+        if partialBoard.count(players['blue']) == 4:
+            score += 500
+        elif partialBoard.count(players['blue']) == 3 and partialBoard.count(0.0) == 1:
+            score += 5
+        elif partialBoard.count(players['blue']) == 2 and partialBoard.count(0.0) == 2:
+            score =+ 2
+        
+
+        if partialBoard.count(players['red']) == 4:
+            score -= 50
+        elif partialBoard.count(players['red']) == 3 and partialBoard.count(0.0) == 1:
+            score -= 5
+        
+
+
+        return score
+
+    def validLocation(self, board):
+        '''returns all valid column values that are considered legal moves
+
+            args:
+                board (mxn matrix): only accepts values 0 to 4
+            
+            Returns:
+                validLocations (list): list of all legal moves in the form of their column value
+        '''
+        validLocations = []
+        for col in range(columns):
+            dumb = board[col]
+            if dumb[0] == 0:
+                validLocations.append(col)
+        return validLocations
+
+    def bestMove(self, board, player):
+        '''Takes in scores from all columns and returns the column that gives the highest score
+            (depreciated since implementation of miniMax algorithm)
+
+            args:
+                board (mxn matrix): only accepts values 0 to 4
+                player(str): value of player ('red' or 'blue')
+            
+            Returns:
+                bestColumn (int): the column that yielded the highest score
+        '''
+        validLocations = self.validLocation(board)
+        bestScore = 0
+        # print(validLocations)
+        bestColumn = random.choice(validLocations)
+        for col in validLocations:
+            tmpBoard = board.copy()
+            count = tmpBoard[col]
+            poop = -1
+            while count[poop] != 0:
+                poop -= 1
+                # self.printBoard()
+            count[poop] = players[player]
+            score = self.boardScore(tmpBoard, turn)
+            if score > bestScore:
+                bestScore = score
+                bestColumn = col
+        # print(bestColumn + 1)
+        return bestColumn
+
+    def findTerminalNode(self, board):
+        '''determine if node is terminal or not
+
+            args:
+                board (mxn matrix): only accepts values 0 to 4
+
+            returns:
+                bool
+        '''
+        return game.findWinner(board, 'red') or game.findWinner(board, 'blue') or len(self.validLocation(board)) == 0
+
+    def miniMaxAlgo(self, board, depth, alpha, beta, maximizingPlayer):
+        '''the minimax algorithm creates a series of nodes of possible moves, 
+        generating a score for each outcome depth number of moves in the future.
+
+            args:
+                board (mxn matrix): only accepts values 0 to 4
+                depth (int): number or nodes created into the future
+                alpha (int): used to stop unneccessary nodes
+                beta (int): used to stop unneccessary nodes
+                maximizingPlayer (bool): whether the currect player is maximising or not
+            
+            Returns:
+                tuple (
+                    bescolumn (int): the column with the highest score from the future
+                    value (int): the value of the score from the future (debugging purposes only)
+                )
+        '''
+        validLocations = self.validLocation(board)
+        # print(validLocations)
+        terminal = self.findTerminalNode(board)
+        if depth == 0 or terminal:
+            if terminal:
+                if game.findWinner(board, 'blue'):
+                    return (None, 99999999)
+                elif game.findWinner(board, 'red'):
+                    return (None, -9999999)
+                else:
+                    return (None, 0)
+            else:
+                return (None, self.boardScore(board, 'blue'))
+        if maximizingPlayer:
+            value = -math.inf
+            bescolumn = random.choice(validLocations)
+            for col in validLocations:
+                tmpBoard = board.copy()
+                count = tmpBoard[col]
+                poop = -1
+                while count[poop] != 0:
+                    poop -= 1
+                    # self.printBoard()
+                count[poop] = players['blue']
+                newScore = self.miniMaxAlgo(tmpBoard, depth-1, alpha, beta, False)[1]
+                if newScore > value:
+                    value = newScore
+                    bescolumn = col
+                alpha = max(alpha, value)
+                if alpha >= beta:
+                    break
+            # print(bescolumn, value)
+            return bescolumn, value
+        else:
+            value = math.inf
+            bescolumn = random.choice(validLocations)
+            for col in validLocations:
+                tmpBoard = board.copy()
+                count = tmpBoard[col]
+                poop = -1
+                while count[poop] != 0:
+                    poop -= 1
+                    # self.printBoard()
+                count[poop] = players['red']
+                newScore = self.miniMaxAlgo(tmpBoard, depth-1, alpha, beta, True)[1]
+                if newScore < value:
+                    value = newScore
+                    bescolumn = col
+                    beta = min(beta, value)
+                    if alpha >= beta:
+                        break
+            # print(bescolumn, value)
+            return bescolumn, value
+            
+        
+            
+
 # contains functions to run connect
 class game:
 
     def matPlot(self, currentBoard):
+        '''Plots a graph of the currentBoard using matplotlib
+
+            args:
+                currectBoard (mxn matrix): only accepts values 0 to 4
+        '''
         plt.imshow(currentBoard)
         cmap = colors.ListedColormap(['#202020', '#ac4141', '#6c99ba', '#7d0a0a', '#093e63'])
         plt.style.use("dark_background")
@@ -27,7 +235,14 @@ class game:
         plt.show()
 
     def findWinner(self, board, player):
-        '''Loops through the board and finds groups of 4'''
+        '''Loops through the board and finds groups of 4
+
+            args:
+                board (mxn matrix): only accepts values 0 to 4
+                player (str): used to referance similar items in matrix
+            returns:
+                bool
+        '''
         # Check horizontal locations for win
         # global board
         for y in range(rows-3):
@@ -77,7 +292,7 @@ class game:
         '''calls user input to select a column w/ missinput checking'''
         while True:
             try:
-                self.selectedColumn = int(input('{}\'s turn (1-7): '.format(turn))) - 1
+                self.selectedColumn = int(input('{}\'s turn (1-{}): '.format(turn.capitalize(), columns))) - 1
                 if self.selectedColumn not in range(0, columns):
                     raise IndexError
                 return self.selectedColumn
@@ -103,7 +318,7 @@ class game:
         # print('  '.join(map(str, range(columns))))
         # print('  1  2  3  4  5  6  7')
         print(np.fliplr(np.rot90(board, k = 3)))
-        self.matPlot(np.fliplr(np.rot90(board, k = 3)))
+        # self.matPlot(np.fliplr(np.rot90(board, k = 3)))
 
     def checkPlayer(self, column, player):
         '''check if player column choice is viable and call for input again if not'''
@@ -129,7 +344,8 @@ class game:
         if self.findWinner(board, turn) is True:
             # print('\033c')
             self.printBoard()
-            print('{} won! Congratulations!'.format(turn))
+            self.matPlot(np.fliplr(np.rot90(board, k = 3)))
+            print('{} won! Congratulations!'.format(turn.capitalize()))
             
             restart = input('Play Again? Y/n: ').lower() or 404
             if restart == 'y' or restart == 404:
@@ -139,15 +355,21 @@ class game:
         return
 
 
+
 if __name__ == "__main__":
     # initiates class and containes main game loop
     game = game()
+    mm = miniMax()
     board = game.initiateBoard()
     turn = 'red'
     while True:
         # print('\033c')
         game.printBoard()
-        # game.matPlot(np.fliplr(np.rot90(board, k = 3)))
+        game.matPlot(np.fliplr(np.rot90(board, k = 3)))
         game.flag = False
-        game.checkPlayer(int(game.selectColumn()), turn)
+        if turn == 'red':
+            game.checkPlayer(int(game.selectColumn()), turn)
+        else:
+            game.insertPlayer(mm.miniMaxAlgo(board, 6, -math.inf, math.inf, True)[0], turn)
+            
         turn = 'blue' if turn == 'red' else 'red'
--- programming/python/rand.py
+++ programming/python/rand.py
@@ -1,31 +1,6 @@
-def timesTables():
-    #let's write a simple program that plots a times table
+players = {
+    "red": 1,
+    "blue": 2
+}
 
-    #initialize the first number
-    a = 1
-
-    #loop over the first number
-    while (a < 10):
-        #initialize the second number
-        b = 1
-        #loop over the second number (so long as it's not bigger than the first)
-        while (b <= a):
-            #work out the answer
-            c = a * b
-            #format the printing according to whether the answer has two digits or one
-            if (c >= 10):
-                print(a, 'x', b, ' = ', c)
-            else:
-                print(a, 'x', b, ' =  ', c)
-
-            #increment the second number
-            b = b + 1
-        #increment the first number
-        a = a + 1
-
-def quadFormula(a,b,c):
-    x1 = (-b + (b**2.0 - 4.0 * a * c)**0.5) / (2.0 * a)
-    x2 = (-b - (b**2.0 - 4.0 * a * c)**0.5) / (2.0 * a)
-    return(x1, x2)
-
-print(quadFormula(a = float(input("a = ")), b = float(input("b = ")), c = float(input("c = "))))
+print(players['red'])
